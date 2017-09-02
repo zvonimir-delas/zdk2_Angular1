326a708930cb6f01d57626369e162a82c60a79e9
zvonimir-delas <6d.solin@gmail.com>
2017-09-02 15:37:06 +0200

    Included scripts for new files

--- scripts/app/app.js
+++ scripts/app/app.js
@@ -2,5 +2,5 @@ var app = angular.module('myApp', ['LocalStorageModule', 'ui.router']);
 
 app.run(function(localStorageService){
     if(!localStorageService.get('students'))
-        localStorageService.set('students', [{name: 'Marko', surname: 'Marković', sex: 'M', dateOfEntry: new Date(2010, 5, 15, 8, 50, 51)}, {name: 'Jelena', surname: 'Jelenić', sex: 'F', dateOfEntry: new Date(2017, 4, 12, 15, 40, 1)}]);  
+        localStorageService.set('students', [{id: 0, name: 'Marko', lastName: 'Marković', grade: 3}, {id: 1, name: 'Jelena', lastName: 'Jelenić', grade: 5}]);  
 });
