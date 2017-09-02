b232500d37c8b7f59ac1e81a4836015b7225626b
zvonimir-delas <6d.solin@gmail.com>
2017-09-02 20:35:16 +0200

    Added data validation

--- scripts/app/add/add.route.js
+++ scripts/app/add/add.route.js
@@ -11,7 +11,7 @@ angular.module('myApp').config(function($stateProvider){
                 <input type="text" ng-model="lastName"></input>
                 <div>Grade</div>
                 <input type="radio" ng-model="grade" value="1">1 <input type="radio" ng-model="grade" value="2">2 <input type="radio" ng-model="grade" value="3">3 <input type="radio" ng-model="grade" value="4">4 <input type="radio" ng-model="grade" value="5">5<br>
-                <button ng-click="addNewStudent()">Add</button>
+                <button ng-click="addNewStudent()" ng-disabled="!(name && lastName && grade)">Add</button>
                 <br><br>
                 <button ui-sref="listAll">Back</button>
                 `
