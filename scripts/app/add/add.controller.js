47ba4e171fd237bcfa07e5d97baabd6087e3e87c
zvonimir-delas <6d.solin@gmail.com>
2017-09-02 20:37:04 +0200

    Fixed system of assigning unique ID's (previous would create a conflict if a student was deleted, except if it was the last added student)

--- scripts/app/add/add.controller.js
+++ scripts/app/add/add.controller.js
@@ -1,13 +1,26 @@
 angular.module('myApp').controller('addController', function($scope, $stateParams, localStorageService){
-
-
+    
+    $scope.allStudents = localStorageService.get('studentsZD_App2');
+    
     $scope.addNewStudent = function(){
-        allStudents = localStorageService.get('studentsZD_App2');
-        allStudents.push({name: $scope.name, lastName: $scope.lastName, grade: $scope.grade, id: allStudents.length});
-        localStorageService.set('studentsZD_App2', allStudents);
+        $scope.allStudents.push({name: $scope.name, lastName: $scope.lastName, grade: $scope.grade, id: $scope.getUniqueId()});
+        localStorageService.set('studentsZD_App2', $scope.allStudents);
 
         $scope.name = null;
         $scope.lastName = null;
         $scope.grade = null;
+    }
+
+    $scope.getUniqueId = function(){
+        var biggestId = 0;
+
+        $scope.allStudents.forEach(function(student) {
+            
+            if(biggestId < student.id)
+                biggestId = student.id;
+
+        }, this);
+;
+        return (biggestId+1);
     }
 })
